460e9bd06a4ac922ffaf77709c62d71636e78759
Muhammet Taha GENC <mtahagenc@gmail.com>
2020-02-24 23:50:44 +0300

    WhatsApp Clone

---
+++ Flash Chat/WelcomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat
+//
+//  This is the welcome view controller - the first thign the user sees
+//
+
+import UIKit
+
+
+
+class WelcomeViewController: UIViewController {
+
+   
+    @IBAction func goToLogin(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
